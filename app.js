714799be4b3bf2039e242370890c315c8e2a7e99
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-03-26 15:21:28 -0700

    change to animation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const flightPath = {
-    curviness:1.25,
+    curviness:1,
     autoRotate:true,
     values:[
         {x:100,y:-20},
@@ -8,7 +8,8 @@ const flightPath = {
         {x:750, y:-100},
         {x:350,y:-50},
         {x:600,y:100},
-        {x:800, y:0}
+        {x:800, y:0},
+        {x:window.innerWidth, y:150}
     ]
 };
 
@@ -19,7 +20,7 @@ const flightPath = {
 
 const tween = new TimelineLite();
 tween.add(
-    TweenLite.to(".paper-plane", 1, {
+    TweenLite.to(".paper-plane", 2.5, {
         bezier:flightPath,
         ease:Power1.easeInOut
     })
