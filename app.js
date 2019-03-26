c91378d24add71f42ec5f3d5483406100db4024b
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-03-26 15:10:26 -0700

    added plugins, basic animation fixed

---
+++ app.js
@@ -0,0 +1,26 @@
+const flightPath = {
+    curviness:1.25,
+    autoRotate:true,
+    values:[
+        {x:100,y:-20},
+        {x:300, y:10},
+        {x:500, y:100},
+        {x:750, y:-100},
+        {x:350,y:-50},
+        {x:600,y:100},
+        {x:800, y:0}
+    ]
+};
+
+// TweenMax.to(".paper-plane", 1.25,{x:100, y:-20});
+// TweenMax.to(".paper-plane", 1.25,{x:300, y:10});
+// TweenMax.to(".paper-plane", 1.25,{x:500, y:100});
+// TweenMax.to(".paper-plane", 1.25,{x:750, y:-100});
+
+const tween = new TimelineLite();
+tween.add(
+    TweenLite.to(".paper-plane", 1, {
+        bezier:flightPath,
+        ease:Power1.easeInOut
+    })
+)
